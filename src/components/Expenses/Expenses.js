b994f9d6f68e77208da26c4767626e4cd9c274b3
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-08-31 15:11:53 +0200

    deliver a messsage if zero expense this year : 3 ways to achieve

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {createFactory, useState} from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
@@ -17,20 +17,56 @@ const Expenses = (props) => {
     item.date.getFullYear() === parseInt(filteredYear,10)
   );
   
+  // cf(***)
+  let expensesContent = <p>No Expense found</p>
+
+  if(filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((item) => 
+      <ExpenseItem
+          key={item.id} 
+          title={item.title}
+          amount = {item.amount}
+          date={item.date} />
+    )
+  }
+
+
+
   return(
     <Card className="expenses">
       <ExpensesFilter
         onChangeFilter={filterChangeHandler}
         selected={filteredYear} />
-  
-      {filteredExpenses.map((item) => 
-        <ExpenseItem
-            key={item.id} 
-            title={item.title}
-            amount = {item.amount}
-            date={item.date} />
-        )
-      }
+      
+      {/* If no Expense during a year, let's a message       */}
+          {/* first with a ternary statement : */}
+
+              {/* {filteredExpenses.length === 0 ? (
+                <p>No Expense found</p>
+              ) : (
+                filteredExpenses.map((item) => 
+                  <ExpenseItem
+                      key={item.id} 
+                      title={item.title}
+                      amount = {item.amount}
+                      date={item.date} />
+                )
+              )} */}
+
+          {/* with a javascipt tricks to more readable code */}
+          {/* on utilise ici l'opératuer &&. Si la première condition est vérifiée, alors l'expression retournée est celle qui vient juste après le && */}
+      {/* {filteredExpenses.length === 0 && <p>No Expenses found</p>}
+      {filteredExpenses.length > 0 && filteredExpenses.map((item) => 
+          <ExpenseItem
+              key={item.id} 
+              title={item.title}
+              amount = {item.amount}
+              date={item.date} />
+        )} */}
+    {/* maintenant on assaye d'extraire la logique de la fonction return() cf((***)) */}
+
+    {expensesContent}
+
     </Card>
   );
 }
