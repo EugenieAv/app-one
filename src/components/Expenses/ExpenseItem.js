a208503d8e5cd624f16df74816a784da2152cabb
Eugenie Avedikian <eugenieavedikian@gmail.com>
2021-08-31 15:37:49 +0200

    refacto avec creation du composant expensesList

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,14 +12,17 @@ const ExpenseItem = (props) => {
         setTitle('updated!')
         console.log(title);
     }
-    return (    
-        <Card className="expense-item" key={props.id}>
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>  
-            <button onClick={clickHandler}>Change Title</button> 
-        </Card>);
+    return (   
+        <li>
+            <Card className="expense-item" key={props.id}>
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>  
+                <button onClick={clickHandler}>Change Title</button> 
+            </Card>
+        </li> 
+    );
 }
 export default ExpenseItem; 
